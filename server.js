9e7be80824a079295064243917ed57bd6f68eb27
8frank8 <frank9g7@gmail.com>
2021-05-06 20:29:43 -0500

    Update server.js
    my first change

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
-const express = require (express)
-const app = express();
+const express = require("express")
+
+const articleRouter = require("./routes/articles")
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use( '/article' , articleRouter)
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title:'test article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title:'test article 2',
+        createdAt:  new Date(),
+        description: 'Test description'
+    }
+    
+    ]
+
+    res.render('articles/index', {articles: articles})
+})
+
+app.listen(5000)
